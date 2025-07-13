c64c6666bcb3b164581622749960e14224dc8b73
Sean <Sean_Cox@student.uml.edu>
2025-07-13 19:51:00 -0400

    compeleted simple tokenzier

--- tokenizer.py
+++ tokenizer.py
@@ -52,6 +52,72 @@ for i, item in enumerate(vocab.items()):
     print(item)
     if i >= 20:
         break
+# ('\n', 0)... ('Alienation', 18) ('All', 19) ('Always', 20)
 
+# Lets make a class
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()} # Reverse tokenization
+
+    def encode(self, text):
+        char_count = Counter(text)
+        special_chars = [key for key in char_count.keys() if not key.isalpha()]
+        pattern = '|'.join(re.escape(c) for c in special_chars)
+        preprocessed = re.split(f'({pattern})', text)
+        preprocessed = [c for c in preprocessed if c] # Remove '' values
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, text):
+        text = "".join([self.int_to_str[i] for i in text])
+        return text
+    
+tokenizer = SimpleTokenizerV1(vocab)
+text = """Out in the mindless void the daemon bore me,
+Past the bright clusters of dimensioned space,
+Till neither time nor matter stretched before me,
+But only Chaos, without form or place."""
+
+ids = tokenizer.encode(text)
+print(ids)
+# [164, 1, 876, 1, 1430, 1... 7]
+print(tokenizer.decode(ids)) 
+# Out in the mindless void the daemon bore me...
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()} # Reverse tokenization
+
+    def encode(self, text):
+        char_count = Counter(text)
+        special_chars = [key for key in char_count.keys() if not key.isalpha()]
+        special_chars = [item for item in special_chars if item not in ['<', '>', '|']]
+        pattern = '|'.join(re.escape(c) for c in special_chars)
+        preprocessed = re.split(f'({pattern})', text)
+        preprocessed = [c for c in preprocessed if c] # Remove '' values
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" 
+                        for item in preprocessed] # Add processing for unknown words
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, text):
+        text = "".join([self.int_to_str[i] for i in text])
+        return text
+
+vocab_words.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token : integer for integer, token in enumerate(vocab_words)}
+print(vocab["<|endoftext|>"])
+text1 = "Out in the mindless void the daemon bore me"
+text2 = "I heard all things in the heaven and in the earth. I heard many things in hell."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+# Out in the mindless void the daemon bore me <|endoftext|> I heard all things in the heaven and in the earth. I heard many things in hell.
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+# [164, 1, 876, 1, 1430, 1, 1016, 1, 1550...]
+print(tokenizer.decode(tokenizer.encode(text)))
 
 
